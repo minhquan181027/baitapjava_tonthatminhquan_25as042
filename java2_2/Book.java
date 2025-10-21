ec29ba78c64b177eaa9274acdd2eb7ae521b8b61
minhquan <minhquan181027@gmail.com>
2025-10-21 10:46:47 +0700

    baitapjava

---
+++ java2_2/Book.java
@@ -0,0 +1,59 @@
+package java2_2;
+
+import java.util.Arrays;
+
+public class Book {
+	private String name;
+	private Author[] authors;
+	private double price;
+	private int qty;
+
+	public Book(String name, Author[] authors, double price) {
+		super();
+		this.name = name;
+		this.authors = authors;
+		this.price = price;
+	}
+
+	public Book(String name, Author[] authors, double price, int qty) {
+		super();
+		this.name = name;
+		this.authors = authors;
+		this.price = price;
+		this.qty = qty;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+	public Author[] getAuthors() {
+ return this.authors;}
+	
+
+	@Override
+	public String toString() {
+		return "Book [name=" + name + ", authors=" + Arrays.toString(authors) + ", price=" + price + ", qty=" + qty
+				+ "]";
+	}
+
+	public String getAuthorNames() {
+		String s="";
+		for(int i=0;i<authors.length;i++) {
+			s+=authors[i].getName()+",";
+		}
+		return s;
+	}
+}
